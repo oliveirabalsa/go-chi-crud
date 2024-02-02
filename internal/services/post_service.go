c57a02b56ab973c59d1f6ac5c0c0563c912f6316
Leonardo <oliveirabalsa2@gmail.com>
2024-02-02 19:13:11 -0300

    feat(service): add createPost method

--- internal/services/post_service.go
+++ internal/services/post_service.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/oliveirabalsa/go-simple-crud/internal/entities"
+import (
+	"github.com/google/uuid"
+	"github.com/oliveirabalsa/go-simple-crud/internal/entities"
+)
 
 type PostService struct {
 }
@@ -9,12 +12,16 @@ func NewPostService() *PostService {
 	return &PostService{}
 }
 
-func GetPost() entities.Post {
+func (ps *PostService) GetPost() entities.Post {
 	post := entities.Post{
 		Title:       "Lorem impsum",
 		Description: "Lorem ipmsum iamet",
 	}
-
 	return post
+}
 
+func (ps *PostService) CreatePost(post entities.Post) (entities.Post, error) {
+	// do something
+	post.Id = uuid.New()
+	return post, nil
 }
