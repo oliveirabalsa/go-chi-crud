c1e19605601f3bbc7a1fed03633d7cfa480bdeac
Leonardo <oliveirabalsa2@gmail.com>
2024-02-02 19:12:43 -0300

    feat(entity): add post entity

--- internal/entities/post_entity.go
+++ internal/entities/post_entity.go
@@ -1,6 +1,21 @@
 package entities
 
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+)
+
 type Post struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Id          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+}
+
+func (p *Post) ToJson() []byte {
+	data, err := json.Marshal(p)
+	if err != nil {
+		return nil
+	}
+	return data
 }
