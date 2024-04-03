7352dd112bfa92c6990a21746b4453762f9f45ca
Leonardo Oliveira Balsalobre <leonardo@leonardo>
2024-04-03 19:38:32 -0300

    feat: add db config

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,23 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/oliveirabalsa/go-simple-crud/internal/config"
 	"github.com/oliveirabalsa/go-simple-crud/internal/handlers"
 	"github.com/oliveirabalsa/go-simple-crud/internal/repositories"
 	"github.com/oliveirabalsa/go-simple-crud/internal/services"
 )
 
 func main() {
-	postRepository := repositories.NewPostRepository()
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	db := config.InitDB()
+
+	postRepository := repositories.NewPostRepository(db)
 	postService := services.NewPostService(*postRepository)
 	postHandler := handlers.NewPostHandler(*postService)
 
