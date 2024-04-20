837e3b50da5d2e65c7b8e2a8accf72867ca5fd7b
Leonardo Oliveira Balsalobre <leonardo@leonardo>
2024-04-20 01:32:58 -0300

    feat(handler): add update and getbyid

--- internal/handlers/post_handler.go
+++ internal/handlers/post_handler.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/oliveirabalsa/go-simple-crud/internal/entities"
 	"github.com/oliveirabalsa/go-simple-crud/internal/services"
 )
@@ -22,30 +24,79 @@ func (h *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	post := h.postService.GetPost()
 	jsonData, err := json.Marshal(post)
 	if err != nil {
-		http.Error(w, "Error marshaling JSON", http.StatusInternalServerError)
+		http.Error(w, "Error marshaling JSON", http.StatusBadRequest)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonData)
 }
 
+func (h *PostHandler) GetPostById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	parsedId, err := uuid.Parse(id)
+	if err != nil {
+		http.Error(w, "Error parsing Id", http.StatusBadRequest)
+		return
+	}
+	post, err := h.postService.GetByID(parsedId)
+	if err != nil {
+		http.Error(w, "Post not found", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(post.ToJson())
+
+}
+
 func (h *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
 	var post = entities.Post{}
 
 	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
-		http.Error(w, "Error decoding JSON", http.StatusInternalServerError)
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
 		return
 	}
 
 	data, err := h.postService.CreatePost(post)
 	if err != nil {
-		http.Error(w, "Error creating post", http.StatusInternalServerError)
+		http.Error(w, "Error creating post", http.StatusBadRequest)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	w.Write(data.ToJson())
+}
 
-	// get
+func (h *PostHandler) UpdatePost(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	var post = entities.Post{}
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	parsedId, err := uuid.Parse(id)
+
+	if err != nil {
+		http.Error(w, "Error parsing Id", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.postService.GetByID(parsedId)
+
+	if err != nil {
+		http.Error(w, "Post not found", http.StatusNotFound)
+		return
+	}
+
+	post.Id = parsedId
+
+	updatedPost, err := h.postService.UpdatePost(post)
+	if err != nil {
+		http.Error(w, "Error updating post", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(updatedPost.ToJson())
 }
