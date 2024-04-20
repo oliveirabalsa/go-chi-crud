9e4f65f5b551d8ee96daa004ddd08a18ce801d4f
Leonardo Oliveira Balsalobre <leonardo@leonardo>
2024-04-20 01:33:17 -0300

    feat(services): add update and getbyid

--- internal/services/post_service.go
+++ internal/services/post_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 	"github.com/oliveirabalsa/go-simple-crud/internal/entities"
 	"github.com/oliveirabalsa/go-simple-crud/internal/repositories"
@@ -23,5 +25,20 @@ func (ps *PostService) GetPost() []entities.Post {
 func (ps *PostService) CreatePost(post entities.Post) (entities.Post, error) {
 	post.Id = uuid.New()
 	return ps.postRepository.CreatePost(post), nil
+}
 
+func (ps *PostService) GetByID(id uuid.UUID) (entities.Post, error) {
+	post, err := ps.postRepository.GetPostById(id)
+	if err != nil {
+		return entities.Post{}, err
+	}
+	if post == (entities.Post{}) {
+		return entities.Post{}, fmt.Errorf("post with ID %s not found", id)
+	}
+	fmt.Println(post)
+	return post, nil
+}
+
+func (ps *PostService) UpdatePost(post entities.Post) (entities.Post, error) {
+	return ps.postRepository.UpdatePost(post), nil
 }
